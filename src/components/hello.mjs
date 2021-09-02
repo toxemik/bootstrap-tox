89135e9754532bdbf4732356a806f49a81482494
toxemik <toxemik@gmail.com>
2021-09-02 18:56:29 +0200

    initial commit; add bootstrap assests and link; declare bootstrap components as mjs; use bootstrap cover template in index.mjs; replace % values with viewport values (vw, vh)

---
+++ src/components/hello.mjs
@@ -0,0 +1,7 @@
+const data = { text: "Hello World!" }
+
+const template = `
+        <p style="color: blue">${ data.text }</p>
+    `
+
+export default template
---
+++ src/components/pageHeader.mjs
@@ -0,0 +1,11 @@
+const data = {
+
+}
+
+const template = `
+    <div class="page-header">
+        <h1>Example page header <small>Subtext for header</small></h1>
+    </div>
+`
+
+export default template
